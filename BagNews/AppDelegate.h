c34e75d4d2b9e957558e69dcc7d425573de35c78
微凉 <weiliang@weiliangdemacbook-air.local>
2016-04-13 18:33:15 +0800

    Initial Commit

---
+++ BagNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BagNews
+//
+//  Created by 微凉 on 16/4/13.
+//  Copyright © 2016年 微凉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BagNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BagNews
+//
+//  Created by 微凉 on 16/4/13.
+//  Copyright © 2016年 微凉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
